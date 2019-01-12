4eeb82a31e3318ab63f1d1c45d7cb7aae2ce2d5f
LennonMb <makinaface@gmail.com>
2019-01-12 12:14:36 -0500

    Update getcomics.info.js

--- getcomics.info.js
+++ getcomics.info.js
@@ -1,3 +1,7 @@
+// https://getcomics.info/tag/action-comics/
+// https://getcomics.info/tag/superman/
+// https://getcomics.info/tag/doomsday-clock/
+
 clear()
 Array.from(document.querySelectorAll(".aio-pulse a"))
 .map(function(a,i){
