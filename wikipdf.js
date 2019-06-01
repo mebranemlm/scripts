2f60a75e482a770f12d83f866a41611d1f27904b
LennonMb <makinaface@gmail.com>
2019-06-01 12:52:45 -0500

    Update wikipdf.js

--- wikipdf.js
+++ wikipdf.js
@@ -1,2 +1,3 @@
-wikitxt.trim().split("\n").filter((v,i,a)=>a.indexOf(v)==i && v.trim()!="").map((w)=>{return "https://en.wikipedia.org/api/rest_v1/page/pdf/"+w.split("#")[0].split("/").reverse()[0]
+var t=wikitxt.trim().split("\n").filter((v,i,a)=>a.indexOf(v)==i && v.trim()!="").map((w)=>{return "https://en.wikipedia.org/api/rest_v1/page/pdf/"+w.split("#")[0].split("/").reverse()[0]
 }).join("\n")
+console.log(t)
