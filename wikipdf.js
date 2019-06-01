1ca78ce125771073013f74db12eefd1e76936752
LennonMb <makinaface@gmail.com>
2019-06-01 12:51:50 -0500

    Create wikipdf.js

---
+++ wikipdf.js
@@ -0,0 +1,2 @@
+wikitxt.trim().split("\n").filter((v,i,a)=>a.indexOf(v)==i && v.trim()!="").map((w)=>{return "https://en.wikipedia.org/api/rest_v1/page/pdf/"+w.split("#")[0].split("/").reverse()[0]
+}).join("\n")
