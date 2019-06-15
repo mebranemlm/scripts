08a84177536b10acedce9773f4af85e42476d71e
LennonMb <makinaface@gmail.com>
2019-06-15 13:35:24 -0500

    Create google-search.js

---
+++ google-search.js
@@ -0,0 +1,18 @@
+//https://www.google.com/search?q=site:biblicalarchaeology.org
+
+var arr=[]
+
+var divs=Array.from(document.querySelectorAll(".r"))
+divs.forEach((div,i,a)=>{
+ var a=div.querySelectorAll("a")[0]
+ var link=a.href
+ var h3= a.querySelectorAll("h3")[0]
+ if(h3){
+   var text =h3.innerText.replace(/\|/g,"-")
+   arr.push(`${link} | ${text}`)
+ }
+
+})
+
+arr.sort().join("\n")
+
