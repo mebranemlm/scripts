8a1a091366289cf7733d63f8ae3f08c4d4e18154
LennonMb <makinaface@gmail.com>
2019-06-23 13:09:39 -0500

    Update libgen.js

--- libgen.js
+++ libgen.js
@@ -1,13 +1,30 @@
 clear()
+var n=1
 var books= Array.from(document.querySelectorAll("a[title]"))
 .filter((v,i)=>{
 	return (false
 || v.innerHTML=="[1]"
-|| v.innerHTML=="[2]"
-|| v.innerHTML=="[3]"
-|| v.innerHTML=="[4]"
-|| v.innerHTML=="[5]")
+// || v.innerHTML=="[2]"
+// || v.innerHTML=="[3]"
+// || v.innerHTML=="[4]"
+//|| v.innerHTML=="[5]"
+)
 })
 .map(function(v,i){return v.href})
-.sort().reverse().join("\n")
-console.log(books)
+.map((v,i)=>{
+  return !v.includes("http://libgen.io/book/index.php?") ? v : v.replace(
+    "http://libgen.io/book/index.php?",
+    "http://booksdescr.org/ads.php?")
+})
+.sort().reverse()
+
+var arrnum = books.length
+var div = 10
+var ini = div*(n-1)
+var end= div*(n)
+
+var portion = books.slice(ini, end)
+console.log(portion.join("\n"))
+console.log(portion.length)
+
+ 
