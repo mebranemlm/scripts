025cff4f280a687ffdb3739a2f26eaa2d6c53fe3
LennonMb <makinaface@gmail.com>
2019-07-03 14:38:30 -0500

    Update wikipdf.js

--- wikipdf.js
+++ wikipdf.js
@@ -1,6 +1,15 @@
+var dir="D:/wkd/pdf"
+
 var t=wikitxt.trim().split("\n")
 .map((v,i)=> v.split("|||")[0])
 .filter((v,i,a)=>a.indexOf(v)==i && v.trim()!="")
-.map((w)=>{return "https://en.wikipedia.org/api/rest_v1/page/pdf/"+w.split("#")[0].split("/").reverse()[0]
-}).join("\n")
-console.log(t)
+.map((w)=>{
+var name=w.split("#")[0].split("/").reverse()[0].trim()
+
+return `wget "https://en.wikipedia.org/api/rest_v1/page/pdf/${name}" -O "${dir}/${name}.pdf"`
+})
+t=t.reverse()
+t.push(`mkdir ${dir}`)
+t=t.reverse()
+t.push("exit\n")
+console.log(t.join("\n"))
