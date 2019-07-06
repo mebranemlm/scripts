b16e3b27cfc17a80b2206b7faaff36363b20dfa9
LennonMb <makinaface@gmail.com>
2019-07-06 12:36:51 -0500

    Update google-search.js

--- google-search.js
+++ google-search.js
@@ -10,10 +10,8 @@ divs.forEach((div,i,a)=>{
  var h3= a.querySelectorAll("h3")[0]
  if(h3){
    var text =h3.innerText.replace(/\|/g,"-")
-   arr.push(`${link} | ${text}`)
+   if(text.trim()!="") arr.push(`${link} ||| ${text}`)
  }
 
 })
-
 console.log(arr.join("\n"))
-
