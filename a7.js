37858fba42f3694cbc352a7c5a937172a4f11ede
LennonMb <makinaface@gmail.com>
2019-10-11 13:01:36 -0500

    Create a7.js

---
+++ a7.js
@@ -0,0 +1,32 @@
+function checkOptions(){
+
+document.querySelectorAll("input[type=checkbox]").forEach(
+(c)=>{
+
+var txt=c.parentNode.parentNode.children[0].innerHTML.toLowerCase();
+console.log(txt,c.checked)
+
+if(txt.includes("english") || txt.includes("spanish") || txt.includes("no hearing")){
+   if (!c.checked) c.click();
+   console.log(c.checked);  
+}})
+}
+
+var i=setInterval(()=>{
+checkOptions();
+},1000);
+
+setTimeout(()=>{
+clearInterval(i);
+
+document.querySelectorAll(".epeven input[type=checkbox]").forEach(function(c){
+c.click();
+});
+
+setTimeout(()=>{
+ document.querySelector("input[name=Submit]").click();
+},1000);
+ 
+},3000);
+
+
